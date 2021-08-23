c33766d75749fb2f80a7478f58ef55e7b670e40a
RogerioAguilera <raguileraribeiro33@gmail.com>
2021-08-23 09:38:06 -0400

    Novos ajustes

--- src/test/java/qa/automation/group/GetBooksTest.java
+++ src/test/java/qa/automation/group/GetBooksTest.java
@@ -1,10 +1,14 @@
 package qa.automation.group;
 
+import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
 public class GetBooksTest {
 
+    @Test
     public void autenticarTest(){
 
-        int code=RestAssured.given()
+        int code= RestAssured.given()
         .auth().preemptive()
         .basic("admin1","test")
         .when("localhost:8080/books")
